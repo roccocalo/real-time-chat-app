4e42028db16ce76a9a85e2c2bcbd4e58d92431ef
roccocalo <roccocalo03@gmail.com>
2024-09-08 16:28:33 +0200

    implement logic and front-end

---
+++ src/main/java/com/roccocalo/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.roccocalo.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/roccocalo/chat/config/WebSocketEventListener.java
@@ -0,0 +1,37 @@
+package com.roccocalo.chat.config;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.roccocalo.chat.chat.ChatMessage;
+import com.roccocalo.chat.chat.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void HandleWebSocketDisconnectListener(
+        SessionDisconnectEvent event
+    ) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+            .type(MessageType.LEAVE)
+            .sender(username)
+            .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
